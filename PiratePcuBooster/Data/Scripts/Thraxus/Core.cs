e56b84fe1b7a82340a08df0342f768491ee3ec0a
Thraxus <mornadak@hotmail.com>
2020-04-25 00:25:10 -0400

    Add project files.

---
+++ PiratePcuBooster/Data/Scripts/Thraxus/Core.cs
@@ -0,0 +1,21 @@
+﻿using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+
+namespace PiratePcuBooster.Thraxus
+{
+	[MySessionComponentDescriptor(MyUpdateOrder.NoUpdate, priority: int.MinValue + 1)]
+    public class Core : MySessionComponentBase
+    {
+	    public override void LoadData()
+	    {
+		    base.LoadData();
+		    if (MyAPIGateway.Session.SessionSettings.BlockLimitsEnabled == MyBlockLimitsEnabledEnum.NONE)
+			    MyAPIGateway.Session.SessionSettings.PiratePCU = 1000000;
+		    else
+		    {
+			    MyAPIGateway.Session.SessionSettings.PiratePCU = MyAPIGateway.Session.SessionSettings.TotalPCU * 3;
+		    }
+		}
+    }
+}
